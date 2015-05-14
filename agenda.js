4533a4762f6124a9753a3ee5e15c3f831982dea6
safirelauene <safire.lauene@gmail.com>
2015-05-14 15:47:45 -0300

    arquivo json

---
+++ agenda.js
@@ -0,0 +1,6 @@
+{"employees":[
+	{"funcionario":"Safire","ramal":"7370","telefone":"(12)981188323","departamento":"Imprensa"},
+	{"funcionario":"Ricardo","ramal":"7443","telefone":"(12)988421269","departamento":"Imprensa"},
+	{"funcionario":"Thaís","ramal":"7328","telefone":"","departamento":"Imprensa"},
+	{"funcionario":"Marrey","ramal":"7337","telefone":"","departamento":"Imprensa"}
+	]}
